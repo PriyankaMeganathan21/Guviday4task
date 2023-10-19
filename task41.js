2a78a95688d8b8f0b217c90ddc4f9bd69334412c
PriyankaMeganathan21 <115691627+PriyankaMeganathan21@users.noreply.github.com>
2023-10-19 12:43:25 +0530

    Delete task41.js

--- task41.js
+++
@@ -1,7 +0,0 @@
-var obj1 = {name: "Person 1", age:5};
-var obj2 = {age:5, name: "Person 1"};
-
-var obj3 = Object.assign(obj1,obj2);
-console.log(obj3)
-
-console.log(JSON.stringify(obj1) == JSON.stringify(obj3));
